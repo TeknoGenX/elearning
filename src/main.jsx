9dd0a99ecc7e095031a0e913c23140c3bd652146
Andi Liani <teknogenx@gmail.com>
2026-01-30 18:41:31 +0700

    Fix: Switch to HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import Login from './pages/Login.jsx';
@@ -19,7 +19,7 @@ import ModuleEditor from './pages/ModuleEditor.jsx'; // Import ModuleEditor
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename="/elearning/">
+    <Router>
       <AuthProvider>
         <ModuleProvider>
           <Routes>
